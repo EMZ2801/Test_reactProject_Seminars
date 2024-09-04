7609538c0018c66bdc0aa1ae6ed6c29c23727870
EMZ2801 <Ekaterina.dubei@mail.ru>
2024-09-04 09:19:31 +0300

    used callback function

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
 
 function App() {
   const [posts, setPosts] = useState ([
@@ -14,39 +15,13 @@ function App() {
     {id: 3, title: 'Javascript 3', body:'Description'}
   ]);
   
-  const [post, setPost] = useState ({title:"", body:""});
-
-
-  const addNewPost = (e) => {
-    e.preventDefault();
-    
-    setPosts ([...posts, {...post, id: Date.now()}]);
-    setPost ({title:"", body:""});
-  }
+ const createPost = (newPost) =>
+  setPosts ([...posts, newPost])
 
   return (
     <div className="App">
-      <form>
-        {/*управляемый компонент*/}
-          <MyInput 
-          value={post.title}
-          onChange={e => setPost({...post, title: e.target.value})}
-          type='text'
-          placeholder="Название поста"/>
-
-          {/* управляемый компонент */}
-          <MyInput 
-          value={post.body}
-          onChange={e => setPost({...post, body: e.target.value})}
-          type='text'
-          placeholder="Описание поста" />
-
-          <MyButton onClick={addNewPost}> Создать пост </MyButton>
-      </form>
-
-
+      <PostForm create={createPost}/>
       <PostList posts={posts} title='Посты про JS'/>
-      {/* <PostList posts={posts2} title='Посты про Pyton'/> */}
 
     </div>
   );
