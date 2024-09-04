f90ff097766415252656b735842883de26fe457e
EMZ2801 <Ekaterina.dubei@mail.ru>
2024-09-04 12:03:01 +0300

    added sorting (drop-down list)

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 function App() {
   const [posts, setPosts] = useState ([
@@ -15,6 +16,7 @@ function App() {
     {id: 3, title: 'Javascript 3', body:'Description'}
   ]);
   
+  const [selectedSort, setSelectedSort] = useState('')
  const createPost = (newPost) =>
   setPosts ([...posts, newPost])
 
@@ -24,9 +26,27 @@ function App() {
   setPosts(posts.filter(p => p.id !== post.id))
  }
 
+ const sortPosts = (sort) => {
+  setSelectedSort(sort);
+    console.log(sort);
+ }
   return (
     <div className="App">
       <PostForm create={createPost}/>
+        <hr style={{margin:"15px"}}></hr>
+        <div>
+          <MySelect 
+            value={setSelectedSort}
+            onChange={sortPosts}
+            defaultValue="Сортировка"
+            options={[
+              {value:"title", name: "По названию"},
+              {value:"body", name: "По описанию"},
+            ]}
+          />
+        </div>
+
+
       {posts.length !==0
         ? <PostList remove={removePost} posts={posts} title='Посты про JS'/>
         : <h1 style={{textAlign: 'center'}}>
