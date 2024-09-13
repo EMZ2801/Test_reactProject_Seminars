9a37c7d84c96253ca7dc747543f3373ef10effac
EMZ2801 <Ekaterina.dubei@mail.ru>
2024-09-13 12:52:44 +0300

    Added a custom hook useFetching(). Error Handling

--- src/App.js
+++ src/App.js
@@ -14,13 +14,18 @@ import { usePosts } from "./hooks/usePosts";
 import axios from "axios";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
+import { useFetching } from "./hooks/useFetching";
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
   const sortedAndSearcedPosts = usePosts(posts, filter.sort, filter.query);
-  const [isPostLoading, setIsPostsLoading] = useState(false);
+
+  const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
+    const posts = await PostService.getAll();
+    setPosts(posts);
+  });
 
   useEffect(() => {
     fetchPosts();
@@ -30,15 +35,6 @@ function App() {
     setPosts([...posts, newPost]);
     setModal(false);
   };
-
-  async function fetchPosts() {
-    setIsPostsLoading(true);
-    setTimeout(async () => {
-      const posts = await PostService.getAll();
-      setPosts(posts);
-      setIsPostsLoading(false);
-    }, 1000);
-  }
 
   //получаем post из дочернего компонента
   const removePost = (post) => {
@@ -58,6 +54,7 @@ function App() {
       <hr style={{ margin: "15px 0" }} />
       <PostFilter filter={filter} setFilter={setFilter} />
 
+      {postError && <h1> Произошла ошибка ${postError}</h1>}
       {isPostLoading ? (
         <div
           style={{ display: "flex", justifyContent: "center", marginTop: 50 }}
