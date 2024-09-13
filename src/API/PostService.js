45afd3b52922eb56c50bbb9b0abd6060ca7a667a
EMZ2801 <Ekaterina.dubei@mail.ru>
2024-09-13 14:27:51 +0300

    Organized a page-by-page output. Pagination

--- src/API/PostService.js
+++ src/API/PostService.js
@@ -1,10 +1,16 @@
 import axios from "axios";
 
 export default class PostService {
-  static async getAll() {
+  static async getAll(limit: number = 10, page: number = 2) {
     const response = await axios.get(
-      "https://jsonplaceholder.typicode.com/posts"
+      "https://jsonplaceholder.typicode.com/posts",
+      {
+        params: {
+          _limit: limit,
+          _page: page,
+        },
+      }
     );
-    return response.data;
+    return response;
   }
 }
