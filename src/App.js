6deca6816d54797fc64eb3a252efde016f2f4cf3
EMZ2801 <Ekaterina.dubei@mail.ru>
2024-09-13 15:11:56 +0300

    Edited page numbering (pagination)

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import axios from "axios";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
 import { useFetching } from "./hooks/useFetching";
-import { getPageCount } from "./utils/pages";
+import { getPageCount, getPagesArray } from "./utils/pages";
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -25,11 +25,8 @@ function App() {
   const [limit, setLimit] = useState(10);
   const [page, setPage] = useState(1);
   const sortedAndSearcedPosts = usePosts(posts, filter.sort, filter.query);
-  let pagesArray = [];
-  for (let i = 0; i < totalPages; i++) {
-    pagesArray.push(i + 1);
-  }
-  console.log(pagesArray);
+  let pagesArray = getPagesArray(totalPages);
+
   const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
     const response = await PostService.getAll(limit, page);
     setPosts(response.data);
@@ -81,6 +78,18 @@ function App() {
           title="Посты про JS"
         />
       )}
+      <div className="page__wrapper">
+        {pagesArray.map((p) => (
+          <span
+            onClick={() => setPage(p)}
+            key={p}
+            className={page === p ? "page page__current" : "page"}
+          >
+            {" "}
+            {p}
+          </span>
+        ))}
+      </div>
     </div>
   );
 }
