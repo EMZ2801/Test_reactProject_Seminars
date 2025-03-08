80c40799797ab75baaf75750c2cb18c4901fff56
EMZ2801 <Ekaterina.dubei@mail.ru>
2025-03-08 17:33:04 +0300

    edited the rendering of the selected post

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -3,14 +3,17 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import About from '../pages/About'
 import Posts from '../pages/Posts'
 import Error from '../pages/Error'
+import PostIdPage from './PostIdPage'
 
 const AppRouter = () => {
   return (
       <Routes>
         <Route path="/about" element={<About />} />
-        <Route path="/posts" element={<Posts />} />
-        <Route path="/error" element={<Error/>} />
+        <Route exact path="/posts" element={<Posts />} />
+        <Route exact path="/posts/:id" element={<PostIdPage />} />
+        <Route path="/" element={<Posts />} />
         <Route path="*" element={<Navigate to="/error" />} />
+        <Route path="/error" element={<Error/>} />
       </Routes>
   )
 }
